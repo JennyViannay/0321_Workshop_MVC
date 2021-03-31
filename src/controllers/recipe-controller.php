823190e78984fec573759d87d42dcad442bc056a
JennyViannay <jenny.viannay75@gmail.com>
2021-03-31 16:16:50 +0200

    step 2

--- src/controllers/recipe-controller.php
+++ src/controllers/recipe-controller.php
@@ -17,5 +17,27 @@ function showRecipe(int $id): void
         header('Location: /');
     }
 
-    require __DIR__.'/../views/show.php';
+    require __DIR__ . '/../views/show.php';
+}
+
+function addRecipe(): void
+{
+    $errors = [];
+    if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+        $recipe = "";
+        if (!empty($_POST['title']) && !empty($_POST['description'])) {
+            $recipe = array_map("trim", $_POST);
+            if (strlen($recipe['title']) > 4) {
+                $errors[] = "Max lenght error";
+            }
+        } else {
+            $errors[] = "Tous les champs sont requis";
+        }
+
+        if (empty($errors)) {
+            saveRecipe($recipe);
+            header('Location: /');
+        }
+    }
+    require __DIR__ . '/../views/form.php';
 }
